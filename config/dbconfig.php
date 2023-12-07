218cf8129b48fdaf92545a3dfa90843546d541b2
Hojoong-Rome <hojoong9919@naver.com>
2023-12-07 18:21:31 +0900

    fist

---
+++ config/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+    define('MYSQL_HOST', 'localhost');
+    define('MYSQL_USER', 'root');
+    define('MYSQL_PASS', '080401familyrome*');
+    define('MYSQL_DB', 'mybbs');
+    define('MYSQL_SET_MODE', true);
+?>
